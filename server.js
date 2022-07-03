268ea21261d5c5ee9186d81701625b9fc472f6e3
Kevin Miller <100856999+kevinmilleryo@users.noreply.github.com>
2022-07-03 09:41:18 -0400

    added cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const MongoClient = require('mongodb').MongoClient
+const cors = require ('cors')
 const PORT = 2121
 require('dotenv').config()
 
@@ -19,6 +20,7 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(cors())
 
 
 
